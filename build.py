d42a8c7a4aaebe529bb931a5d289d291dda957eb
theirix <theirix@gmail.com>
2018-02-06 00:20:53 +0300

    Build webp and openjpeg enabled builds too on unix

--- build.py
+++ build.py
@@ -15,6 +15,15 @@ if __name__ == "__main__":
         if not (platform.system() == "Windows" and item.settings["compiler"] == "gcc" and
                 item.settings["arch"] == "x86"):
             items.append(item)
+        # add full-options build for selected platforms
+        if platform.system() != "Windows" and item.settings["arch"] == "x86_64" and \
+                item.settings["build_type"] == "Release":
+            new_options = copy.copy(item.options)
+            new_options["leptonica:with_webp"] = True
+            new_options["leptonica:with_openjpeg"] = True
+            items.append([item.settings, new_options, item.env_vars,
+                item.build_requires, item.reference])
+
     builder.items = items
 
     builder.run()
